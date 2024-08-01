6dff38403424033b4464310356e8308c815e08cf
jubygeorge <jubieegeorge@gmail.com>
2024-08-01 13:49:44 +0100

    Second Commit

--- BDDCucumber/src/test/java/pageObjects/HomePage.java
+++ BDDCucumber/src/test/java/pageObjects/HomePage.java
@@ -1,11 +1,14 @@
 package pageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import factory.BaseClass;
 
 public class HomePage  extends BaseClass {
 	WebDriver driver;
+	
+	By lnkSignIn  = By.linkText("Sign In ");
 
 	public HomePage(WebDriver driver) {
 		this.driver = driver;
